922b1bb474b78ee6f82bc49a434a68822e6614a1
Mauricio Vieira dos Santos <mauricio.vieiradossantos@mosolf.de>
2024-09-30 11:39:07 +0000

    create first model on controller

--- webapp/controller/App.controller.ts
+++ webapp/controller/App.controller.ts
@@ -1,10 +1,23 @@
 import Controller from "sap/ui/core/mvc/Controller";
 import MessageToast from "sap/m/MessageToast";
+import JSONModel from "sap/ui/model/json/JSONModel";
+import Model from "sap/ui/model/Model";
 
 /**
  * @namespace santos.sapui5ts.controller
  */
 export default class App extends Controller {
+
+    onInit(): void {
+        let oClient: object = {
+            client: {
+                name: "Santos"
+            }
+        };
+
+        let oModel: Model = new JSONModel(oClient);
+        this.getView()?.setModel(oModel);
+    }
 
     onSayHello(): void {
         MessageToast.show("hello");
