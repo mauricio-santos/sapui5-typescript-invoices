78c5cb997a8bad402c1616df3ccacb616e47ecec
Mauricio Vieira dos Santos <mauricio.vieiradossantos@mosolf.de>
2024-09-30 09:36:45 +0000

    create frist view from index.ts

--- webapp/index.ts
+++ webapp/index.ts
@@ -1,5 +1,10 @@
 import Text from "sap/m/Text";
+import XMLView from "sap/ui/core/mvc/XMLView";
 
 new Text({
     text: "Hello World from index.ts"
 }).placeAt("content");
+
+XMLView.create({
+    viewName: "santos.sapui5ts.view.App"
+}).then((oView): XMLView => oView.placeAt("content"))
