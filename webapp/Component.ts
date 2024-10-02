a57710fbec3a6372c6e69bf56bbc08b5681a0213
Mauricio Vieira dos Santos <mauricio.vieiradossantos@mosolf.de>
2024-10-02 14:04:08 +0000

    Component use manifest.json

--- webapp/Component.ts
+++ webapp/Component.ts
@@ -1,7 +1,4 @@
-import Control from "sap/ui/core/Control";
 import UIComponent from "sap/ui/core/UIComponent";
-import ResourceModel from "sap/ui/model/resource/ResourceModel";
-import XMLView from "sap/ui/core/mvc/XMLView";
 import ClientsModel from "./model/ClientsModel";
 
 /**
@@ -12,7 +9,8 @@ export default class Component extends UIComponent {
     public static metadata = {
         "interfaces": [
             "sap.ui.core.IAsyncContentCreation" //This allows the component to be generated asynchronously
-        ]
+        ],
+        "manifest": "json"
     };
 
     init(): void {
@@ -20,23 +18,8 @@ export default class Component extends UIComponent {
         // call the init function of the parent
         super.init();
 
-        //set model i18n
-        const i18nModel = new ResourceModel({
-            bundleName: "santos.sapui5ts.i18n.i18n"
-        });
-        this.setModel(i18nModel, "i18n");
-
         //set model Clients
         const oClients = (new ClientsModel).create();        
         this.setModel(oClients);
     };
-
-    //method for create the view as we did in the index.ts file to set our app view as the root view of the component
-    createContent(): Control | Promise<Control | null> | null {
-        return XMLView.create({
-            "viewName": "santos.sapui5ts.view.App",
-            "id": "app"
-        });
-    };
-
 }
