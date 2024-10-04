352273001f3b997d28b60a13ece93e02a27f7c90
Mauricio Vieira dos Santos <mauricio.vieiradossantos@mosolf.de>
2024-10-04 08:26:51 +0000

    Create view HelloPanel and using XMLVIew

--- webapp/controller/App.controller.ts
+++ webapp/controller/App.controller.ts
@@ -1,8 +1,4 @@
 import Controller from "sap/ui/core/mvc/Controller";
-import MessageToast from "sap/m/MessageToast";
-import ResourceModel from "sap/ui/model/resource/ResourceModel";
-import JSONModel from "sap/ui/model/json/JSONModel";
-import ResourceBundle from "sap/base/i18n/ResourceBundle";
 
 /**
  * @namespace santos.sapui5ts.controller
@@ -11,13 +7,5 @@ export default class App extends Controller {
 
     public onInit(): void {
 
-    }
-
-    public onSayHelloButtonPress(): void {
-        const oBudle = (<ResourceBundle>(<ResourceModel>this.getView()?.getModel("i18n"))?.getResourceBundle());
-        const oClientModel = (<JSONModel>this.getView()?.getModel())?.getProperty("/client/name")        
-        const msg = oBudle.getText("msgHelloClient", [oClientModel]) || 'no text defined';
-
-        MessageToast.show(msg);
     }
 }
---
+++ webapp/controller/HelloPanel.controller.ts
@@ -0,0 +1,23 @@
+import Controller from "sap/ui/core/mvc/Controller";
+import MessageToast from "sap/m/MessageToast";
+import ResourceModel from "sap/ui/model/resource/ResourceModel";
+import JSONModel from "sap/ui/model/json/JSONModel";
+import ResourceBundle from "sap/base/i18n/ResourceBundle";
+
+/**
+ * @namespace santos.sapui5ts.controller
+ */
+export default class HelloPanel extends Controller {
+
+    onInit(): void {
+        
+    }
+    
+    public onSayHelloButtonPress(): void {
+        const oBudle = (<ResourceBundle>(<ResourceModel>this.getView()?.getModel("i18n"))?.getResourceBundle());
+        const oClientModel = (<JSONModel>this.getView()?.getModel())?.getProperty("/client/name")        
+        const msg = oBudle.getText("msgHelloClient", [oClientModel]) || 'no text defined';
+
+        MessageToast.show(msg);
+    }
+}
