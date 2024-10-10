ead3918047d7b6aecb734bcb2451d2ebbab41a42
Mauricio Vieira dos Santos <mauricio.vieiradossantos@mosolf.de>
2024-10-10 09:50:33 +0000

    Filter

--- webapp/controller/InvoiceList.controller.ts
+++ webapp/controller/InvoiceList.controller.ts
@@ -1,5 +1,9 @@
 import Controller from "sap/ui/core/mvc/Controller";
 import JSONModel from "sap/ui/model/json/JSONModel";
+import { SearchField$SearchEvent } from "sap/ui/commons/SearchField";
+import Filter from "sap/ui/model/Filter";
+import FilterOperator from "sap/ui/model/FilterOperator";
+import ListBinding from "sap/ui/model/ListBinding";
 
 /**
  * @namespace santos.sapui5ts.controller
@@ -12,4 +16,17 @@ export default class InvoiceList extends Controller {
         });        
         this.getView()?.setModel(currencyModel, "currencyModel")
     }
+	onFilterInvoices(event: SearchField$SearchEvent): void {        
+		//build filter array
+        const filter = [];
+        const query = event.getParameter("query");
+
+        query && filter.push(new Filter("ProductName", FilterOperator.Contains, query));
+
+        //filter binding
+        const list = this.byId("idInvoicesList");
+        const binding = list?.getBinding("items") as ListBinding;
+        binding?.filter(filter);
+	}
 }
+
