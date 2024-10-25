a370c21bc81194ebb277d97741658e05dcc1b184
Mauricio Vieira dos Santos <mauricio.vieiradossantos@mosolf.de>
2024-10-25 09:51:06 +0000

    Log Level - debug

--- webapp/controller/HelloPanel.controller.ts
+++ webapp/controller/HelloPanel.controller.ts
@@ -4,6 +4,7 @@ import ResourceModel from "sap/ui/model/resource/ResourceModel";
 import JSONModel from "sap/ui/model/json/JSONModel";
 import ResourceBundle from "sap/base/i18n/ResourceBundle";
 import Component from "../Component";
+import Log from "sap/base/Log";
 
 /**
  * @namespace santos.sapui5ts.controller
@@ -12,6 +13,17 @@ export default class HelloPanel extends Controller {
 
     onInit(): void {
         
+    }
+
+    onBeforeRendering(): void | undefined {
+        window.message = "Message from OnBeforeRendering";
+        Log.info(window.message);
+        Log.error(window.message);
+        
+    }
+
+    onAfterRendering(): void | undefined {
+        // debugger
     }
     
     public onSayHelloButtonPress(): void {
